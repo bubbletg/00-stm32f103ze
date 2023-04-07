3cedffd7eee50e03fd4ba9a9bc98d7636b8b7e5b
BubbleTg <tg_z@qq.com>
2023-04-07 11:30:10 +0800

    feat: save

--- Drivers/SYSTEM/usart/usart.c
+++ Drivers/SYSTEM/usart/usart.c
@@ -11,25 +11,6 @@
 /* 加入以下代码, 支持printf函数, 而不需要选择use MicroLIB */
 
 #if 1
-
-#if (__ARMCC_VERSION >= 6010050)            /* 使用AC6编译器时 */
-__asm(".global __use_no_semihosting\n\t");  /* 声明不使用半主机模式 */
-__asm(".global __ARM_use_no_argv \n\t");    /* AC6下需要声明main函数为无参数格式，否则部分例程可能出现半主机模式 */
-
-#else
-/* 使用AC5编译器时, 要在这里定义__FILE 和 不使用半主机模式 */
-#pragma import(__use_no_semihosting)
-
-struct __FILE
-{
-    int handle;
-    /* Whatever you require here. If the only file you are using is */
-    /* standard output using printf() for debugging, no file handling */
-    /* is required. */
-};
-
-#endif
-
 /* 不使用半主机模式，至少需要重定义_ttywrch\_sys_exit\_sys_command_string函数,以同时兼容AC6和AC5模式 */
 int _ttywrch(int ch)
 {
@@ -55,8 +36,9 @@ FILE __stdout;
 /* MDK下需要重定义fputc函数, printf函数最终会通过调用fputc输出字符串到串口 */
 int fputc(int ch, FILE *f)
 {
+	  // SR 状态寄存器 位6(01000000 即0X40) 为发送完成标志位
     while ((USART1->SR & 0X40) == 0);     /* 等待上一个字符发送完成 */
-
+		// DR 数据寄存器
     USART1->DR = (uint8_t)ch;             /* 将要发送的字符 ch 写入到DR寄存器 */
     return ch;
 }
--- Drivers/SYSTEM/usart/usart.h
+++ Drivers/SYSTEM/usart/usart.h
@@ -8,8 +8,7 @@
 extern uint8_t G_USART_RX_STA;
 
 // 串口接收缓冲区
-extern uint8_t G_USART_RX_BUF[USART_REC_LEN]; // 接收缓冲,最大USART_REC_LEN个字节.
-
+extern uint8_t G_USART_RX_BUF[1]; // 接收缓冲
 // UART 句柄
 extern UART_HandleTypeDef G_UART_InitStruct;
 
